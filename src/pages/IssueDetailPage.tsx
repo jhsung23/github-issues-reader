2ab5cb4de86b2914268e7d698a5aa4fbbacc8dcb
sungjihyun <jhsung23@gmail.com>
2023-08-30 15:09:02 +0900

    feat: 이슈 상세 페이지

--- src/pages/IssueDetailPage.tsx
+++ src/pages/IssueDetailPage.tsx
@@ -1,5 +1,32 @@
+import styled from 'styled-components';
+import { useLocation } from 'react-router-dom';
+
+import { IssueInfo } from '@/components/domain/issue';
+import { Issue } from '@/types/issue';
+import { Img } from '@/components/atom';
+import { MarkdownViewer } from '@/components/common';
+
 const IssueDetailPage = () => {
-  return <div>IssueDetailPage</div>;
+  const { state: issue }: { state: Issue } = useLocation();
+
+  return (
+    <Article>
+      <ArticleInfo>
+        <Img src={issue.avatarUrl} height={'50px'} width={'50px'} />
+        <IssueInfo issue={issue} />
+      </ArticleInfo>
+      <MarkdownViewer content={issue.body} />
+    </Article>
+  );
 };
 
 export default IssueDetailPage;
+
+const Article = styled.article`
+  width: 100%;
+`;
+
+const ArticleInfo = styled.div`
+  display: flex;
+  align-items: center;
+`;
