76b7e4cf63bbde61d7cbfa4a30ecd165683c6495
sungjihyun <jhsung23@gmail.com>
2023-08-30 15:04:30 +0900

    feat: 상수, 타입 정리

---
+++ src/constants/constants.ts
@@ -0,0 +1,4 @@
+export const REPO = Object.freeze({
+  OWNER: 'facebook',
+  NAME: 'react',
+});
---
+++ src/types/issue.ts
@@ -0,0 +1,9 @@
+export type Issue = {
+  issueId: number;
+  title: string;
+  userName: string;
+  createdAt: Date;
+  comments: number;
+  avatarUrl: string;
+  body: string;
+};
