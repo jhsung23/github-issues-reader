0a8e249c5eb9e616cead55460fae6084dc5c3163
sungjihyun <jhsung23@gmail.com>
2023-08-30 17:42:18 +0900

    feat: issue 목록 보여주기, 무한스크롤 적용

--- src/pages/IssuePage.tsx
+++ src/pages/IssuePage.tsx
@@ -1,8 +1,53 @@
+import { Fragment } from 'react';
 import styled from 'styled-components';
 
+import useIssueQuery, { IssuesResponseData } from '@/apis/useIssueQuery';
+import { Ad, Loading } from '@/components/common';
+import { IssueListItem, ListFallback } from '@/components/domain/issue';
+import { useIntersectionObserver } from '@/hooks';
+import { Issue } from '@/types/issue';
+
+const PER_LIST = 5;
+
 const IssuePage = () => {
-  return <IssueList></IssueList>;
+  const { issues, isLoading, hasNextPage, fetchMoreIssues } = useIssueQuery();
+  const ref = useIntersectionObserver(fetchMoreIssues, { threshold: 0.3 });
+
+  return (
+    <>
+      {issues.length ? (
+        <>
+          <IssueList>
+            {issues.map((issue, order) => {
+              const filteredIssue = getFilteredIssue(issue);
+              return (
+                <Fragment key={filteredIssue.issueId}>
+                  <IssueListItem issue={filteredIssue} />
+                  {(order + 1) % PER_LIST === 0 && <Ad />}
+                </Fragment>
+              );
+            })}
+          </IssueList>
+          {!isLoading && hasNextPage && <div ref={ref}></div>}
+          {isLoading && <Loading />}
+        </>
+      ) : (
+        <ListFallback />
+      )}
+    </>
+  );
 };
+
+const getFilteredIssue = (issue: IssuesResponseData[number]): Issue => ({
+  issueId: issue.id,
+  issueNumber: issue.number,
+  userName: issue.user ? issue.user.login : '알 수 없음',
+  title: issue.title,
+  createdAt: new Date(issue.created_at),
+  comments: issue.comments,
+  avatarUrl: issue.user ? issue.user.avatar_url : 'https://i.stack.imgur.com/frlIf.png',
+  body: issue.body ?? '',
+});
 
 export default IssuePage;
 
