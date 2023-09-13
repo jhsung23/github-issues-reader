04331345ed9cbe31c3194470a6fdb0dc0ae7b5f9
sungjihyun <jhsung23@gmail.com>
2023-09-13 21:39:03 +0900

    refactor: 이슈 데이터를 전역 상태에 저장 및 파일명 변경

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,2 +1,3 @@
 export { default as useIntersectionObserver } from './useIntersectionObserver';
 export { default as useScrollTop } from './useScrollTop';
+export { default as useIssues } from './useIssues';
