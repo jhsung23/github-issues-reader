bb5ad750ce80914610faad2e6ec15c440dd674d6
sungjihyun <jhsung23@gmail.com>
2023-10-25 20:06:57 +0900

    feat: 단일 issue 가져오기

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,4 @@
 export { default as useIntersectionObserver } from './useIntersectionObserver';
-export { default as useScrollTop } from './useScrollTop';
+export { default as useIssue } from './useIssue';
 export { default as useIssues } from './useIssues';
+export { default as useScrollTop } from './useScrollTop';
