22b3769f218af9af1c539e46e2966a464544fd61
sungjihyun <jhsung23@gmail.com>
2023-10-25 23:40:33 +0900

    feat: 광고 변경

--- src/components/common/Ad.tsx
+++ src/components/common/Ad.tsx
@@ -1,15 +1,13 @@
 import styled from 'styled-components';
 
+import ad from '@/assets/ad.png';
 import { Img } from '@/components/atom';
 
 const Ad = () => {
   return (
     <Container>
-      <a href="https://www.wanted.co.kr/" target="_blank">
-        <Img
-          src="https://image.wanted.co.kr/optimize?src=https%3A%2F%2Fstatic.wanted.co.kr%2Fimages%2Fuserweb%2Flogo_wanted_black.png&w=110&q=100"
-          height={'80px'}
-        />
+      <a href="https://github.com/jhsung23" target="_blank">
+        <Img src={ad} height={'100px'} />
       </a>
     </Container>
   );
